669447dcc07233d44d016cfeddf9824cc935b883
Rohan Bavishi <rbavishi@berkeley.edu>
2019-07-24 16:04:12 -0700

    Renaming Model and Encoding to OpModel and OpEncoder

--- atlas/models/encoding.py
+++ atlas/models/encoding.py
@@ -1,2 +1,2 @@
-class OperatorEncoding:
+class OpEncoder:
     pass
--- atlas/models/model.py
+++ atlas/models/model.py
@@ -1,2 +1,13 @@
-class OperatorModel:
-    pass
+from abc import abstractmethod, ABC
+from typing import Any
+
+from atlas.models.encoding import OpEncoder
+
+
+class OpModel(ABC):
+    def __init__(self, encoder: OpEncoder):
+        self.encoder = encoder
+
+    @abstractmethod
+    def train(self, gen: 'Generator', data: Any):
+        pass
