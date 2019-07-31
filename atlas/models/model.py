2a456bc423104126bd6d236c1133fa1353c6fb7d
Rohan Bavishi <rbavishi@berkeley.edu>
2019-07-31 22:35:20 +0530

    Moving the train API to the model from Generator

--- atlas/models/model.py
+++ atlas/models/model.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABC
 from typing import Any, Collection
 
+from atlas import Generator
 from atlas.models.encoding import OpEncoder
 from atlas.tracing import GeneratorTrace
 
@@ -10,7 +11,17 @@ class OpModel(ABC):
         self.encoder = encoder
 
     @abstractmethod
-    def train(self, gen: 'Generator', data: Any):
+    def train(self, gen: Generator, data: Any):
+        """
+        The entry point for training a generator to bias certain execution paths based on the
+        input and an end objective. This method intends to cover the class of imitation/supervised
+        learning techniques where a model is trained offline on some collected data.
+
+        Args:
+            gen (Generator): The generator to train the model for.
+            data: The data to train the generator on (usually traces of generator executions)
+
+        """
         pass
 
 
@@ -24,6 +35,7 @@ class IndependentOpModel(OpModel):
     def train(self, gen: 'Generator', data: Collection[GeneratorTrace]):
         for d in data:
             for t in d.op_traces:
+                print(t)
                 op_encoder = self.encoder.get_encoder(t.op_name, t.sid, t.oid)
-                encoding = op_encoder(t.domain, t.context)
+                encoding = op_encoder(t.domain, t.context, choice=t.choice, mode='training')
 
