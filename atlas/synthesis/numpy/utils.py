d78f99cfa9fee80b467fcd77b5101762ca0ae1d4
Caroline Lemieux <clemieux@berkeley.edu>
2019-08-05 14:32:48 -0700

    Why am I adding prime factorization utils?

---
+++ atlas/synthesis/numpy/utils.py
@@ -0,0 +1,23 @@
+from math import sqrt, ceil
+
+
+def is_prime(n):
+    if n < 4:
+        return True
+    if n % 2 == 0:
+        return False
+    for d in range(5, ceil(sqrt(n)) + 1, 2):
+        if n % d == 0:
+            return False
+    return True
+
+def get_non_1_prime_factors(n):
+    print("calling for ", n)
+    factor_list = []
+    for d in [2] + list(range(3, ceil(sqrt(n))+1, 2)):
+        m = n
+        while m % d == 0:
+            factor_list.append(d)
+            m = m // d
+    return factor_list
+
