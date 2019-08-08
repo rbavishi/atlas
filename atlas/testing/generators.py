4a0dbeb911ae82cf4b911d342d127b3588e81182
Rohan Bavishi <rbavishi@berkeley.edu>
2019-08-08 13:51:23 -0700

    Adding basic generator unit-tests

---
+++ atlas/testing/generators.py
@@ -0,0 +1,43 @@
+import unittest
+
+from atlas import generator
+from atlas.strategies import DfsStrategy
+
+
+class GeneratorBasic(unittest.TestCase):
+    def test_gen_single_1(self):
+        @generator(strategy='dfs')
+        def binary(length: int):
+            s = ""
+            for i in range(length):
+                s += Select(["0", "1"])
+
+            return s
+
+        self.assertEqual(list(binary.generate(2)), ["00", "01", "10", "11"])
+
+    def test_gen_custom_strategy_1(self):
+        class ReversedDFS(DfsStrategy):
+            def Select_reversed(self, domain, *args, **kwargs):
+                yield from reversed(domain)
+
+        @generator(strategy=ReversedDFS())
+        def binary(length: int):
+            s = ""
+            for i in range(length):
+                s += Select(["0", "1"], oid='reversed')
+
+            return s
+
+        self.assertEqual(list(binary.generate(2)), list(reversed(["00", "01", "10", "11"])))
+
+    def test_gen_composition_1(self):
+        @generator(group='binary')
+        def lower_bit():
+            return Select(["0", "1"])
+
+        @generator(group='binary')
+        def upper_bit():
+            return Select(["0", "1"]) + lower_bit()
+
+        self.assertEqual(list(upper_bit.generate()), ["00", "01", "10", "11"])
