ac9c62cdef07fb1f04699c15434560cb2c9928f2
Rohan Bavishi <rbavishi@berkeley.edu>
2019-08-12 17:52:03 -0700

    Adapting Pandas models to the new API

--- atlas/synthesis/pandas/models.py
+++ atlas/synthesis/pandas/models.py
@@ -1,93 +1,62 @@
 import os
-import pickle
-from abc import ABC
-from typing import Optional, Collection, Any, Dict
+from typing import Collection, Any, Mapping
 
-from atlas.models.imitation import DefaultIndependentOpModel, DefaultOpModel
-from atlas.models.tensorflow import TensorflowModel
-from atlas.models.tensorflow.graphs.gnn import GGNN
+from atlas.models.imitation import IndependentOperatorsModel
 from atlas.models.tensorflow.graphs.operators import SelectGGNN
 from atlas.synthesis.pandas.encoders import PandasGraphEncoder
 from atlas.tracing import OpTrace
 from atlas.utils.ioutils import IndexedFileReader, IndexedFileWriter
 
 
-class BasePandasOperatorModel(DefaultOpModel, ABC):
-    def __init__(self, sid: str, encoder: PandasGraphEncoder, model: GGNN):
-        super().__init__(sid)
-        self.encoder = encoder
-        self.model = model
-
-    def dump_encodings(self, data: Collection[OpTrace], path: str = None):
-        if isinstance(data, IndexedFileReader):
-            if path is None:
-                path = f"{data.path}.encoded"
-
-            if os.path.exists(path):
-                return IndexedFileReader(path)
-
+def dump_encodings(data: Collection[OpTrace], encoder: PandasGraphEncoder, sid: str, path: str = None):
+    if isinstance(data, IndexedFileReader):
         if path is None:
-            path = 'train.pkl'
+            path = f"{data.path}.encoded"
 
-        encoding_file = IndexedFileWriter(path)
-        encoder_func = self.encoder.get_encoder(self.sid)
-        for op in data:
-            encoding_file.append(encoder_func(
-                domain=op.domain,
-                context=op.context,
-                choice=op.choice,
-                sid=op.sid
-            ))
+        if os.path.exists(path):
+            return IndexedFileReader(path)
 
-        encoding_file.close()
-        return IndexedFileReader(path)
+    if path is None:
+        path = 'train.pkl'
 
-    def train(self, train_op_traces: Collection[OpTrace],
-              validation_op_traces: Optional[Collection[OpTrace]] = None,
-              num_epochs: int = 10, **kwargs):
+    encoding_file = IndexedFileWriter(path)
+    encoder_func = encoder.get_encoder(sid)
+    for op in data:
+        encoding_file.append(encoder_func(
+            domain=op.domain,
+            context=op.context,
+            choice=op.choice,
+            sid=op.sid
+        ))
 
-        encoded_train = self.dump_encodings(train_op_traces)
-        if validation_op_traces is not None:
-            encoded_valid = self.dump_encodings(validation_op_traces)
+    encoding_file.close()
+    return IndexedFileReader(path)
+
+
+class PandasSelect(SelectGGNN):
+    def __init__(self, params: Mapping[str, Any], sid: str):
+        super().__init__(params)
+        self.encoder = PandasGraphEncoder()
+        self.sid = sid
+
+    def train(self, training_data: Collection[OpTrace], validation_data: Collection[OpTrace], *args, **kwargs):
+
+        encoded_train = dump_encodings(training_data, self.encoder, self.sid)
+        if validation_data is not None:
+            encoded_valid = dump_encodings(validation_data, self.encoder, self.sid)
         else:
             encoded_valid = None
 
-        self.model.train(encoded_train, encoded_valid, num_epochs=num_epochs)
-
-    def save(self, path: str):
-        super().save(path)
-        self.model.save(f"{path}.tf")
-        with open(f"{path}.encoder", 'wb') as f:
-            pickle.dump(self.encoder, f)
-        with open(f"{path}.metadata", "wb") as f:
-            pickle.dump({'sid': self.sid}, f)
-
-    @classmethod
-    def load(cls, path: str):
-        with open(f"{path}.metadata", 'rb') as f:
-            metadata = pickle.load(f)
-        with open(f"{path}.encoder", 'rb') as f:
-            encoder = pickle.load(f)
-
-        model = TensorflowModel.load(f"{path}.tf")
-        return cls(metadata['sid'], encoder, model)
-
-
-class PandasSelect(BasePandasOperatorModel):
-    def __init__(self, sid: str, encoder: PandasGraphEncoder, model: GGNN = None, config: Dict = None):
-        super().__init__(sid, encoder, model or SelectGGNN(config))
+        super().train(encoded_train, encoded_valid, *args, **kwargs)
 
     def infer(self, domain, context: Any = None, sid: str = ''):
         encoding = self.encoder.get_encoder(self.sid)(domain, context, mode='inference', sid=sid)
-        inference = self.model.infer([encoding])[0]
+        inference = super().infer([encoding])[0]
         print(inference, sid)
         return [val for val, prob in sorted(inference, key=lambda x: -x[1])]
 
 
-class PandasModelBasic(DefaultIndependentOpModel):
-    def __init__(self, path: str = None):
-        super().__init__(path)
-
+class PandasModelBasic(IndependentOperatorsModel):
     def Select_input_selection(self, *args, **kwargs):
         return None
 
@@ -111,4 +80,4 @@ class PandasModelBasic(DefaultIndependentOpModel):
             'num_edge_types': encoder.get_num_edge_types()
         }
 
-        return PandasSelect(sid, encoder, config=config)
+        return PandasSelect(config, sid)
