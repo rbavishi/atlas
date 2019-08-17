4cbea54166547c93340e6980115439a275dfb081
Rohan Bavishi <rbavishi@berkeley.edu>
2019-08-17 16:02:17 -0700

    Adding tests for ordered subsets

--- atlas/models/tensorflow/graphs/tests.py
+++ atlas/models/tensorflow/graphs/tests.py
@@ -1,7 +1,7 @@
 import random
 import unittest
 
-from atlas.models.tensorflow.graphs.operators import SelectGGNN, SubsetGGNN
+from atlas.models.tensorflow.graphs.operators import SelectGGNN, SubsetGGNN, OrderedSubsetGGNN
 
 
 class SanityTests(unittest.TestCase):
@@ -97,7 +97,7 @@ class SanityTests(unittest.TestCase):
                 'nodes': nodes,
                 'edges': edges,
                 'domain': domain,
-                'choices': choices
+                'choice': choices
             }
 
         training = [create_random_datapoint() for i in range(500)]
@@ -115,4 +115,49 @@ class SanityTests(unittest.TestCase):
 
         model = SubsetGGNN(config)
         history = model.train(training, validation, 50)
+        self.assertGreaterEqual(history[-1]['valid_acc'], 0.90)
+
+    def test_ordered_subset_small_1(self):
+        def create_random_datapoint():
+            #  A set of domain nodes, a set of context nodes, and the selected domain nodes have an edge to one of
+            #  the context nodes. The domain nodes also have a direction edge establishing the relative order
+            #  between them. Thus it should be very easy for the network to learn to predict the right node
+            domain = list(range(random.randrange(3, 10)))
+            context = [i + len(domain) for i in range(random.randrange(1, 10))]
+            terminal = len(domain) + len(context)
+            #  The only node feature is whether it's a domain node or a context node
+            nodes = [[0] for _ in domain] + [[1] for _ in context] + [[2]]
+            choices = sorted(random.sample(domain, random.randrange(2, min(4, len(domain)))))
+            choices.append(terminal)
+            matched = random.choice(context)
+            edges = []
+            for choice in choices:
+                edges.extend([(choice, 0, matched), (matched, 0, choice)])
+
+            for i, j in zip(choices, choices[1:]):
+                edges.append((i, 1, j))
+
+            return {
+                'nodes': nodes,
+                'edges': edges,
+                'domain': domain + [terminal],
+                'choice': choices,
+                'terminal': terminal
+            }
+
+        training = [create_random_datapoint() for i in range(500)]
+        validation = [create_random_datapoint() for i in range(50)]
+
+        config = {
+            'node_dimension': 10,
+            'classifier_hidden_dims': [10],
+            'batch_size': 100000,
+            'layer_timesteps': [1, 1],
+            'num_node_features': 3,
+            'num_edge_types': 2,
+            'learning_rate': 0.01
+        }
+
+        model = OrderedSubsetGGNN(config)
+        history = model.train(training, validation, 200)
         self.assertGreaterEqual(history[-1]['valid_acc'], 0.90)
