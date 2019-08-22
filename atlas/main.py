f179d57cf7a05aa195c009010b0c965782abd4ea
Rohan Bavishi <rbavishi@berkeley.edu>
2019-08-22 08:48:38 -0700

    Cleaning up main.py

--- atlas/main.py
+++ atlas/main.py
@@ -1,23 +1,4 @@
-import itertools
-
-from atlas import generator
-from atlas.ops import Select
-from atlas.semantics import DfsSemantics, op_def
-
-
-@generator
-def myexample(length):
-    s = ""
-    for i in range(length):
-        s += Select(["a", "b"])
-
-    return s
-
-
 def run():
     print("# ------------------------------------------------ #")
     print("# Atlas - A Framework for Neural-Backed Generators #")
     print("# ------------------------------------------------ #")
-
-    for i in myexample.generate(3):
-        print(i)
