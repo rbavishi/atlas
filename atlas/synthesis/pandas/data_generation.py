041e006bc0733861a865716a6d49e9d4aa8bc525
Rohan Bavishi <rbavishi@berkeley.edu>
2019-08-30 18:03:34 -0700

    Recording traces as well

--- atlas/synthesis/pandas/data_generation.py
+++ atlas/synthesis/pandas/data_generation.py
@@ -14,9 +14,10 @@ api_gens = {
 def helper_generate_function_arguments(all_inputs, func: str):
     strategy = PandasDataGenerationStrategy(generate_random_dataframe)
     new_inputs = []
-    for output, args in api_gens[func].generate(all_inputs, output=None,
-                                                generated_inputs=new_inputs).with_strategy(strategy):
-        yield output, args, new_inputs
+    for (output, args), trace in api_gens[func].generate(all_inputs, output=None,
+                                                         generated_inputs=new_inputs).with_strategy(
+        strategy).with_tracing():
+        yield output, args, new_inputs, trace
         new_inputs = []
 
 
@@ -30,11 +31,11 @@ def generate_function_arguments(inputs: List, used_intermediates: List, unused_i
 
 def helper_generate_sequence_arguments(func_seq: List[str], inputs: List,
                                        used_intermediates: List, unused_intermediates: List,
-                                       current_program: List):
+                                       current_program: List, traces: List):
     func = func_seq[0]
     unused_ids = {id(i): i for i in unused_intermediates}
-    for output, args, new_inputs in generate_function_arguments(inputs, used_intermediates, unused_intermediates,
-                                                                func):
+    for output, args, new_inputs, trace in generate_function_arguments(inputs, used_intermediates, unused_intermediates,
+                                                                       func):
         new_used = {id(i) for i in args.values() if id(i) in unused_ids}
         still_unused = [i for i in unused_intermediates if id(i) not in new_used]
         program = current_program + [(func, args)]
@@ -42,13 +43,14 @@ def helper_generate_sequence_arguments(func_seq: List[str], inputs: List,
             if len(still_unused) > 0:
                 continue
 
-            yield output, program
+            yield output, program, traces + [trace]
 
         else:
             still_unused.append(output)
             yield from helper_generate_sequence_arguments(func_seq[1:], inputs + new_inputs,
-                                                          used_intermediates + list(new_used), still_unused, program)
+                                                          used_intermediates + list(new_used), still_unused, program,
+                                                          traces + [trace])
 
 
 def generate_sequence_arguments(func_seq: List[str]):
-    yield from helper_generate_sequence_arguments(func_seq, [], [], [], [])
+    yield from helper_generate_sequence_arguments(func_seq, [], [], [], [], [])
