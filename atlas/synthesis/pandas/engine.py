7693a0c2d29d524a9ed059e41905b152bffd9823
Rohan Bavishi <rbavishi@berkeley.edu>
2019-09-01 22:34:21 -0700

    Reproducing sequential enumerator

--- atlas/synthesis/pandas/engine.py
+++ atlas/synthesis/pandas/engine.py
@@ -1,35 +1,49 @@
 from atlas import generator
 from atlas.exceptions import ExceptionAsContinue
 from atlas.synthesis.pandas.checker import Checker
+from atlas.synthesis.pandas.strategies import PandasSynthesisStrategy
 from atlas.utils import get_group_by_name
-from atlas.stubs import Select, Sequence, Subset, OrderedSubset
+from atlas.synthesis.pandas.stubs import *
 import atlas.synthesis.pandas.api
+from typing import List, Set
 
 api_gens = {
     gen.name: gen for gen in get_group_by_name('pandas')
 }
 
 
-@generator(group='pandas', name='pandas_synthesis_brute_force')
-def brute_force_enumerator(inputs, output):
-    func_seq = Sequence(list(api_gens.keys()), max_len=2, oid='func_seq_sequence')
-
-    prog = []
+@generator(name='pandas_sequential_enumerator', strategy=PandasSynthesisStrategy())
+def sequential_enumerator(inputs, output):
+    """
+    First decides the function sequence to explore, and then decides arguments for each function individually.
+    This is the enumerator used in the OOPSLA '19 system.
+    """
+    func_seq: List[str] = Sequence(list(api_gens.keys()), max_len=3, label='function_sequence_prediction')
+    func_args = []
     intermediates = []
+    unused_intermediates: Set[int] = set()
+
     for func in func_seq:
-        func = api_gens[func]
+        func_gen = api_gens[func]
         try:
-            val, args = func(intermediates + inputs, output)
-        except:
-            raise ExceptionAsContinue
-        prog.append((func.name, args))
+            val, args = func_gen(intermediates + inputs, output, unused_intermediates=unused_intermediates)
+        except ExceptionAsContinue:
+            raise
+        # except:
+        #     raise ExceptionAsContinue
+
+        unused_intermediates.add(id(val))
+        for obj in args.values():
+            unused_intermediates.discard(id(obj))
+
         intermediates.append(val)
+        func_args.append(args)
 
-    return intermediates[-1], prog
+    return intermediates[-1], func_seq, func_args
 
 
-def solve(inputs, output):
+def solve(enumerator, inputs, output):
     checker = Checker.get_checker(output)
-    for val, prog in brute_force_enumerator.generate(inputs, output):
+    for val, prog in enumerator.generate(inputs, output):
         if checker(output, val):
             print(prog)
